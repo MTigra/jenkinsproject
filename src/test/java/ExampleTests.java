8981ce3b169ac1ae457e0158808797d4d00a8322
MTigra <tikol2008@gmail.com>
2020-07-05 03:36:01 +0300

    check it out

---
+++ src/test/java/ExampleTests.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class ExampleTests {
+
+    @Test(expected = NullPointerException.class)
+    public void whenExceptionThrown_thenExpectationSatisfied() {
+        String test = null;
+        test.length();
+    }
+}
